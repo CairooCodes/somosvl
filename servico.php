49d6787b3cc8a83d99b30dce3217bdbeed7182e1
Alex-Brendon <alexbrendon11.dev@gmail.com>
2023-03-30 23:18:52 -0300

    ajust faq somosvl

--- servico.php
+++ servico.php
@@ -249,18 +249,18 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 						<div class="max-w-screen-md mx-auto">
 							<div id="accordion-flush" data-accordion="collapse" data-active-classes="text-gray-900" data-inactive-classes="text-gray-500">
 								<h3 id="accordion-flush-heading-11">
-									<button type="button" class="flex items-center justify-between w-full py-5 font-bold text-left text-gray-800 border-b border-gray-200" data-accordion-target="#accordion-flush-body-11" aria-expanded="true" aria-controls="accordion-flush-body-11">
+									<button type="button" class="flex items-center justify-between w-full py-5 font-bold text-left text-gray-800 border-b border-gray-200" data-accordion-target="#accordion-flush-body-11" aria-expanded="false" aria-controls="accordion-flush-body-11">
 										<span>Quais as taxas do cartão de crédito consignado?
 										</span>
-										<svg data-accordion-icon="" class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+										<svg data-accordion-icon="" class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 											<path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
 										</svg>
 									</button>
 								</h3>
-								<div id="accordion-flush-body-11" class="" aria-labelledby="accordion-flush-heading-11">
+								<div id="accordion-flush-body-11" class="hidden" aria-labelledby="accordion-flush-heading-11">
 									<div class="py-5 border-b border-gray-200">
-										<p class="mb-2 ">As taxas do cartão consignado variam de acordo com a instituição financeira que oferece o serviço, mas geralmente são menores do que as cobradas nos cartões de crédito convencionais.
-										</p>
+										As taxas do cartão consignado variam de acordo com a instituição financeira que oferece o serviço, mas geralmente são menores do que as cobradas nos cartões de crédito convencionais.
+										
 									</div>
 								</div>
 
