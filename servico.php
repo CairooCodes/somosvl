3d4976c90f67cbb73750dc525f5f15cd550118a2
Alex-Brendon <alexbrendon11.dev@gmail.com>
2023-04-03 12:23:37 -0300

    ajust color faqs somosvl

--- servico.php
+++ servico.php
@@ -56,9 +56,9 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 						<h2 class="mb-6 text-3xl font-extrabold tracking-tight text-center text-gray-900 lg:mb-8 lg:text-3xl">
 							Perguntas Frequentes</h2>
 						<div class="max-w-screen-md mx-auto">
-							<div id="accordion-flush" data-accordion="collapse" data-active-classes="text-gray-900" data-inactive-classes="text-gray-500">
+							<div id="accordion-flush" data-accordion="collapse" data-active-classes="text-gray-900" data-inactive-classes="text-gray-900">
 								<h3 id="accordion-flush-heading-1">
-									<button type="button" class="flex items-center justify-between w-full py-5 font-bold text-left text-gray-800  border-b border-gray-200" data-accordion-target="#accordion-flush-body-1" aria-expanded="false" aria-controls="accordion-flush-body-1">
+									<button type="button" class="flex items-center justify-between w-full py-5 font-bold text-left text-gray-900  border-b border-gray-200" data-accordion-target="#accordion-flush-body-1" aria-expanded="false" aria-controls="accordion-flush-body-1">
 										<span>
 											Quem pode adiantar o saque-aniversário?
 										</span>
@@ -137,9 +137,9 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 						<h2 class="mb-6 text-3xl font-extrabold tracking-tight text-center text-gray-900 lg:mb-8 lg:text-3xl">
 							Perguntas Frequentes</h2>
 						<div class="max-w-screen-md mx-auto">
-							<div id="accordion-flush" data-accordion="collapse" data-active-classes="text-gray-900" data-inactive-classes="text-gray-500">
+							<div id="accordion-flush" data-accordion="collapse" data-active-classes="text-gray-900" data-inactive-classes="text-gray-900">
 								<h3 id="accordion-flush-heading-5">
-									<button type="button" class="flex items-center justify-between w-full py-5 font-bold text-left text-gray-800  border-b border-gray-200" data-accordion-target="#accordion-flush-body-5" aria-expanded="false" aria-controls="accordion-flush-body-5">Qual é o valor máximo da parcela do Empréstimo Consignado?
+									<button type="button" class="flex items-center justify-between w-full py-5 font-bold text-left text-gray-900  border-b border-gray-200" data-accordion-target="#accordion-flush-body-5" aria-expanded="false" aria-controls="accordion-flush-body-5">Qual é o valor máximo da parcela do Empréstimo Consignado?
 										</span>
 										<svg data-accordion-icon="" class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
 											<path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path>
@@ -247,9 +247,9 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 						<h2 class="mb-6 text-3xl font-extrabold tracking-tight text-center text-gray-900 lg:mb-8 lg:text-3xl">
 							Perguntas Frequentes</h2>
 						<div class="max-w-screen-md mx-auto">
-							<div id="accordion-flush" data-accordion="collapse" data-active-classes="text-gray-900" data-inactive-classes="text-gray-500">
+							<div id="accordion-flush" data-accordion="collapse" data-active-classes="text-gray-900" data-inactive-classes="text-gray-900">
 								<h3 id="accordion-flush-heading-11">
-									<button type="button" class="flex items-center justify-between w-full py-5 font-bold text-left text-gray-800 border-b border-gray-200" data-accordion-target="#accordion-flush-body-11" aria-expanded="false" aria-controls="accordion-flush-body-11">
+									<button type="button" class="flex items-center justify-between w-full py-5 font-bold text-left text-gray-900 border-b border-gray-200" data-accordion-target="#accordion-flush-body-11" aria-expanded="false" aria-controls="accordion-flush-body-11">
 										<span>Quais as taxas do cartão de crédito consignado?
 										</span>
 										<svg data-accordion-icon="" class="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
