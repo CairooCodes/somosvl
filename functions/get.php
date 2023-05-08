dc6036fb2583ef4801a1f96d24cf6f5fab016076
Cairo <cairofelipedev@gmail.com>
2023-05-08 15:56:37 -0300

    Fix home

--- functions/get.php
+++ functions/get.php
@@ -7,27 +7,3 @@ function getAllProducts()
   $stmt->execute();
   return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
-
-function getProducts()
-{
-  global $DB_con;
-  $stmt = $DB_con->prepare("SELECT * FROM products where categorie_id = 1 order by id desc");
-  $stmt->execute();
-  return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
-
-function getProducts2()
-{
-  global $DB_con;
-  $stmt = $DB_con->prepare("SELECT * FROM products where categorie_id = 9 order by id desc");
-  $stmt->execute();
-  return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
-
-function getProducts3()
-{
-  global $DB_con;
-  $stmt = $DB_con->prepare("SELECT * FROM products where categorie_id = 2 order by id desc");
-  $stmt->execute();
-  return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
